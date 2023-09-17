0c463bd0266070952ded7b0d507ededd150e2d63
Utaro Hayashi <utaro.hayashi@gmail.com>
2023-09-17 18:55:58 -0500

    Added preliminary Payment page

---
+++ src/components/Payment.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  CardElement,
+  Elements,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe("YOUR_STRIPE_PUBLISHABLE_KEY");
+
+const CheckoutForm: React.FC = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+    const result = await stripe.createToken(card!);
+
+    if (result.error) {
+      console.error(result.error.message);
+    } else {
+      // Send token to your server or Stripe to create a charge
+      // After successful payment, send a confirmation email
+      fetch("/api/sendConfirmationEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: "customer@email.com" }),
+      });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Pay
+      </button>
+    </form>
+  );
+};
+
+const Payment: React.FC = () => {
+  return (
+    <Elements stripe={stripePromise}>
+      <CheckoutForm />
+    </Elements>
+  );
+};
+
+export default Payment;
